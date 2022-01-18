944e668378e28ca7989c2626b898cbb6fc970e29
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2022-01-17 16:11:16 -0800

    Add `DataAttr` Built Into
    The data attr was built but remove for performance so there this plugin

---
+++ plugins/Dr-DataAttr.js
@@ -0,0 +1,56 @@
+/**
+ * @name DataAttr
+ * @description Add data attributes to elements
+ * @author Dr.Discord
+ * @version 1.0.0
+ * @license MIT
+ * @ignore false
+ */
+
+let release = DiscordNative.app.getReleaseChannel()
+let Flux
+
+let { getCurrentUser } = DrApi.getModule(["getCurrentUser"])
+let { getChannelId } = DrApi.getModule(["getLastSelectedChannelId", "getChannelId"])
+let { getGuildId } = DrApi.getModule(["getLastSelectedGuildId"])
+
+module.exports = new class {
+  get currentUser() { return getCurrentUser() }
+  get channelId() { return getChannelId() }
+  get guildId() { return getGuildId() }
+  get platform() { return window.process?.platform ?? "unknown" }
+
+  Guild_Channel_Attr({ channelId, guildId }) {
+    // Location
+    document.body.setAttribute("data-location", location.pathname) 
+    if (guildId) document.body.setAttribute("data-guild-id", guildId) 
+    else document.body.removeAttribute("data-guild-id")
+    // Channel
+    if (channelId) document.body.setAttribute("data-channel-id", channelId) 
+    else document.body.removeAttribute("data-channel-id")
+  }
+  async onStart() {
+    // Add attributes
+    document.body.setAttribute("data-platform", this.platform) 
+    document.body.setAttribute("data-location", location.pathname) 
+    document.body.setAttribute("data-release", release) 
+    document.body.setAttribute("data-current-user-id", this.currentUser.id) 
+    this.Guild_Channel_Attr({ channelId: this.channelId, guildId: this.currentGuild })
+    // Add listener for channel change
+    Flux = await DrApi.util.waitUntil(DrApi.FluxDispatcher)
+    Flux.subscribe("CHANNEL_SELECT", this.Guild_Channel_Attr)
+  }
+  async onStop() {
+    // Remove attributes
+    for (const data of [
+      "data-guild-id", "data-channel-id", 
+      "data-current-user-id", "data-location", 
+      "data-release", "data-platform"
+    ]) {
+      document.body.removeAttribute(data)
+    }
+    // Remove listener for channel change
+    Flux = await DrApi.util.waitUntil(DrApi.FluxDispatcher)
+    Flux.unsubscribe("CHANNEL_SELECT", this.Guild_Channel_Attr)
+  }
+}
