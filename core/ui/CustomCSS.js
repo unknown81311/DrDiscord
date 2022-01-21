8b84deebe124797045b9ffa46c6e076ff7e385f9
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-01-20 20:04:14 -0800

    Update CustomCSS.js

--- core/ui/CustomCSS.js
+++ core/ui/CustomCSS.js
@@ -24,49 +24,59 @@ module.exports = () => {
   } = require("electron")
   const DataStore = require("../datastore")
   const settings = DataStore("DR_DISCORD_SETTINGS")
-
-
-
   const button = DrApi.getModule(["ButtonColors"]).default
+  const ButtonColors = DrApi.getModule(["ButtonColors"]).ButtonColors
   const Switch = DrApi.getModule("Switch").default
   const Slider = DrApi.getModule.displayName("Slider").default
+  const colorPicker = DrApi.getModule(['CustomColorPicker']).default
   const TextInput = DrApi.getModule("TextInput").default
   const Icons = require("./Icons")
   const Tooltip = DrApi.getModule.prototypes("renderTooltip").default
   const text = DrApi.getModule("ListSectionItem").default
+  const subText = DrApi.getModule("Text").default
+  const markDown = DrApi.getModule(m => m.default?.displayName === "Markdown" && m.default.rules).default
   let FormTitle = DrApi.getModule("FormTitle").default
   const Flex = {
     Child: FlexChild
   } = DrApi.getModule("Flex").default
 
+  function updateCSSS(){
+    let 
+      data = DataStore("DR_DISCORD_SETTINGS").csss,
+      style = '';
+    for (a in data) {
+      style+=`--dr-${a}:${data[a]};`
+    }
+    DrApi.styling.update('csss',`:root{${style}}`)
+  }
 
-  function openSettings(css) {
-    let style = [];
-    this.setVal=(...e)=>{style+=e}
+  function openSettings(css) { 
+    let style = DataStore("DR_DISCORD_SETTINGS").csss||{};
+    setVal=(ID,VAL,DEFAULT)=>{
+      style[ID]=VAL||DEFAULT;
+      DataStore.setData("DR_DISCORD_SETTINGS", "csss", style)
+      updateCSSS();
+    }
     try {
       const settings = css.match(/:root( +|){((.|\n)*)}/)[0].match(/--dr(.*);/g).map(e => e.split(/-|;/).flatMap(h => [h.split(":")[0], h.split(":").slice(1).join(":")]).filter(n => n).slice(1))
-      console.log(settings)
-      const data = DataStore("DR_DISCORD_SETTINGS").csss||DataStore.setData("DR_DISCORD_SETTINGS", "csss", {});
-      
       
       let reactElms = [];
       for (var i = 0; i < settings.length; i++) {
         const set = settings[i]
-        switch (settings[i][0]) {
-          case 'u':
+        switch (set[0]) {
+          case 't':
             {
               reactElms.push(React.createElement("div",{
                 style:{
                   padding:"10px"
                 },
                 children:[
-                  React.createElement(text,{ children: ["value for id:"+settings[i][1]] }),
+                  React.createElement(text,{ children: ["value for:"+set[1]] }),
                   React.createElement(TextInput,{
-                    style:{maxWidth:"70%"},
                     onInput:e=>{
-                      this.setVal(set[1],set[2].replace(/'|"/g,''))
+                      setVal(set[1],e.target.value,set[2])
                     },
-                    placeholder:settings[i][2].replace(/'|"/g,'')
+                    placeholder:style[set[1]]||set[2]
                   })
                 ]
               }))
@@ -74,27 +84,26 @@ module.exports = () => {
             break;
           case 's':
             { 
-              let lastValue=settings[i][settings[i].length-1]
-              let markes=settings[i].slice(0,-1)
+              let lastValue=set[set.length-1]
+              let markes=set.slice(0,-1)
               reactElms.push(React.createElement("div",{
                 style:{
-                  maxWidth:"70%",padding:"10px"
+                  padding:"10px"
                 },
                 children:[
                   React.createElement(text,{
-                    style:{'margin-bottom': '12px'},
-                    children: ["value for id:"+settings[i][1]]}),
+                    children: ["value for:"+set.slice(-2,-1)[0]]
+                  }),
+                  React.createElement('div',{style:{'margin': '8px'}}),
                     React.createElement(Slider,{
-                      initialValue: lastValue,
+                      initialValue: style[set.slice(-2,-1)[0]]||lastValue,
                       defaultValue: lastValue,
-                      markers: settings[i].slice(1,-2),
+                      markers: set.slice(1,-2),
                       stickToMarkers: true,
-                      minValue: settings[i][1],
+                      minValue: set[1],
                       maxValue: lastValue,
                       handleSize: 10,
-                      onValueChange: (e)=>{
-                        this.setVal(set.splice(-2,-1),e)
-                      }
+                      onValueChange: (e)=> setVal(set.slice(-2,-1)[0],e,lastValue)
                     })
                   ]
               }))
@@ -102,39 +111,140 @@ module.exports = () => {
             break;
           case 'b':
             {
-              console.log('b')
+              reactElms.push(React.createElement("div",{
+                style:{
+                  padding:"10px"
+                },
+                children:[
+                  React.createElement(text,{ children: ["value for:"+set[1]] }),
+                  React.createElement(React.memo(() => {
+                    let [enabled, setEnabled] = React.useState(style[set[1]]===true||parseFloat(set[2]))
+                    return [React.createElement(Switch, {
+                      checked:enabled,
+                      onChange:e=>{
+                        setEnabled(!enabled);
+                        setVal(set[1],enabled,parseFloat(set[2]));
+                      }
+                    })]
+                  }),{})
+                ]
+              }))
             }
             break;
-          case 't':
-            {
-              console.log('t')
-            }
+           case 'h'://hue
+              {
+                // reactElms.push(
+                //   React.createElement("div",{
+                //     style:{
+                //       padding:"10px"
+                //     },
+                //     children: [
+                //     React.createElement(text,{ children: ["value for:"+set[1]] }),
+                //     React.createElement(colorPicker,{
+                //       "position": "right",
+                //       "isPositioned": true,
+                //       // "value": 1752220,
+                //       "colors": [1752220],
+                //       'renderDefaultButton':(e)=>{
+                //         React.createElement(e,{})
+                //       },
+                //       'colorContainerClassName':(e)=>{
+                //         React.createElement(e,{})
+                //       },
+                //       // 'renderDefaultButton':(e)=>{
+                //       //   React.createElement(e,{})
+                //       // },
+                //     })]
+                //   }))
+              }
             break;
         }
       }
-
       try{
-      openModal(props => {
+        // use `"` instead of `'` next time please
+      let moduleID=openModal(props => {
         return React.createElement(ModalRoot, {
           ...props,
-          size: ModalSize.LARGE,
+          size: ModalSize.SMALL,
           children: [
             React.createElement(ModalHeader, {
               separator: false,children: [
-                React.createElement("div", {
-                  className: "Scss-viwer-wrapper",
-                  children: [
-                    'welcom to the thunder dome!'
+                React.createElement(Flex, {
+                  className: "csss-viwer-wrapper",
+                  style:{
+                    width:'100%'
+                  },
+                  children:[
+                    React.createElement(FlexChild, {
+                      style:{
+                        display: 'flex',
+                        'justify-content': 'space-between',
+                        width: '100%'
+                      },
+                      children: 
+                        React.createElement('div',{
+                          children: [
+                            React.createElement('div',{
+                              children: [
+                                React.createElement(FormTitle, {
+                                  children: `Custom Css Settings`,
+                                  tag: FormTitle.Tags.H4
+                                }),
+                                React.createElement(subText,{
+                                  className:"anchor-1MIwyf anchorUnderlineOnHover-2qPutX",
+                                  onClick:()=>{
+                                    DrApi.showConfirmationModal(
+                                      'Tutorial',[
+                                        '⦾ sythax:',
+                                        '\`--dr-<type>-<properties>-<ID>:<default value>;\`',
+                                        '⦾ types:',
+                                        '\`t\` **:** \`adds a setting for a text input\`',
+                                        '\`b\` **:** \`adds a setting for a toggle\`',
+                                        '\`s\` **:** \`adds a setting for a slider\`',
+                                        '\`h\` **:** \`adds a setting for a color picker\`',
+                                        '⦾ how it works:',
+                                        'parses the css and pulls the vareables in the `:root{}` selectors and converts them to a new vareable using the settings.',
+                                        '⦾ examples:',
+                                        '```css\n:root{\n  --dr-t-Text:"Defalt Text";\n}```',
+                                        'this will convert the vareable to `--dr-Text:"Defalt Text";`\n or it will use the value the user inputed in the settings.',
+                                        '```css\n:root{\n  --dr-b-Toggle:1;\n}```',
+                                        'this will convert the vareable to `--dr-Toggle:1`;\n or it will use the value the user inputed in the settings.',
+                                        '```css\n:root{\n  --dr-s-0-1-2-3-4-5-Slider:5;\n}```',
+                                        'this will convert the vareable to `--dr-Slider:5;`\n or it will use the value the user inputed in the settings.',
+                                        '```css\n:root{\n  --dr-h-color:#ffffff;\n}```',
+                                        'this will convert the vareable to `--dr-color:#ffffff;`\n or it will use the value the user inputed in the settings.'
+                                        
+                                        ],{
+                                        cancelText:null, confirmText:'close'
+                                      })
+                                  },
+                                  children:"how does this world?!"
+                                })
+                              ]
+                            }),
+                            React.createElement(button,{
+                              children:'reset',
+                              className:ButtonColors.RED,
+                              onClick:()=>{
+                                DrApi.modal.functions.closeModal(moduleID);
+                                DataStore.setData("DR_DISCORD_SETTINGS", "csss", {});
+                              }
+                            })
+                          ]
+                        })
+                    })
                   ]
                 })
               ]
             }),
-            ...reactElms
+            React.createElement("div",{
+              style:{height: '100%'},
+              children:[...reactElms]
+            })
           ]
         })
       })
       }catch(e){console.log(e)}
-      // DrApi.styling.insert('CustomCssSettings', `:root{${style}}`);
     } catch (e) {
       console.log(e)
     }
@@ -166,12 +276,14 @@ module.exports = () => {
         const value = this.editor.getValue()
         DataStore.setData("DR_DISCORD_SETTINGS", "CSS", value)
         window.document.getElementById("CUSTOMCSS").textContent = styling.compileSass(value)
-        if (global.FloatingCSSEditor) FloatingCSSEditor.setValue(value)
+        if (global.FloatingCSSEditor) FloatingCSSEditor.setValue(value);
+        updateCSSS();
       })
       const contextmenu = this.editor.getContribution('editor.contrib.contextmenu')
       contextmenu._onContextMenu = _ => _
     }
     render() {
+      updateCSSS();
       return (
         this.state.error ? React.createElement("div", null, "An error accord with the monaco editor") : React.createElement("div", {
           style: {
@@ -180,13 +292,14 @@ module.exports = () => {
           },
           children: [
             React.createElement("div", {
-              id: "ligma",
               style: {
                 width: "100%",
                 height: "10%",
+                padding: "3px"
               },
               children: [
                 React.createElement(button, {
+                  className:ButtonColors.GREEN,
                   children: "Open css settings",
                   onClick: () => {
                     openSettings(this.editor.getValue())
